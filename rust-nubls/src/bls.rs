55ad918ea1d80ee9af6687df28209f37981ec780
tuxxy <me@johnpacific.com>
2020-05-12 18:08:42 +0200

    Add tests for signing/verifying and testing the misuse-resistant API

--- rust-nubls/src/bls.rs
+++ rust-nubls/src/bls.rs
@@ -1,4 +1,4 @@
-use bls12_381::G2Affine;
+use bls12_381::{pairing, G1Affine, G2Affine};
 
 use crate::keys::{PrivateKey, PublicKey};
 
@@ -10,16 +10,32 @@ use std::convert::From;
 /// to construct a safe, misuse resistant API by forcing the user to handle
 /// both cases of signature verification logic (Valid/Invalid). This prevents
 /// silent failures that otherwise may be present when APIs return `bool`s.
+#[derive(Debug, Eq, PartialEq)]
 pub enum VerificationResult {
     Valid,
     Invalid,
 }
 
 /// A `Signature` is an Affine element of the G_2 group on the BLS12-381 curve.
+#[derive(Debug, Eq, PartialEq)]
 pub struct Signature(G2Affine);
 
 impl Signature {
-    /// Attempts to verify the signature given a `message` and a `public_key`.
+    /// Creates a `Signature` and returns it by signing the `message_element`
+    /// with the provided `private_key`.
+    ///
+    /// The preferred API to sign messages is in `PrivateKey.sign`.
+    ///
+    /// Presently, the API for hashing to the G_2 group of BLS12-381 is not
+    /// implemented (see https://github.com/nucypher/NuBLS/issues/1). Therefore,
+    /// the message must be prehashed before verification and signing.
+    ///
+    /// TODO: Implement hash_to_curve
+    pub(crate) fn new(private_key: &PrivateKey, message_element: &G2Affine) -> Signature {
+        Signature((message_element * &private_key.0).into())
+    }
+
+    /// Attempts to verify the signature given a `message_element` and a `public_key`.
     /// Returns a `VerificationResult::Valid` if the `message_element` and `public_key`
     /// are correct, and a `VerificationResult::Invalid` if they are not.
     ///
@@ -35,7 +51,10 @@ impl Signature {
         public_key: &PublicKey,
         message_element: &G2Affine,
     ) -> VerificationResult {
-        unimplemented!();
+        let c_1 = pairing(&public_key.0, &message_element);
+        let c_2 = pairing(&G1Affine::generator(), &self.0);
+
+        VerificationResult::from(c_1 == c_2)
     }
 }
 
--- rust-nubls/src/keys.rs
+++ rust-nubls/src/keys.rs
@@ -7,11 +7,11 @@ use std::convert::From;
 
 /// A `PublicKey` represents an Affine element of the G_1 group on the BLS12-381 curve.
 #[derive(Debug, Eq, PartialEq)]
-pub struct PublicKey(G1Affine);
+pub struct PublicKey(pub(crate) G1Affine);
 
 /// A `PrivateKey` represents a Scalar element within the order of the BLS12-381 curve.
 #[derive(Debug, Eq, PartialEq)]
-pub struct PrivateKey(Scalar);
+pub struct PrivateKey(pub(crate) Scalar);
 
 impl PrivateKey {
     /// Generates a random private key and returns it.
@@ -36,7 +36,7 @@ impl PrivateKey {
     ///
     /// TODO: Implement `hash_to_curve` per the IETF hash_to_curve specification.
     pub fn sign(&self, message_element: &G2Affine) -> Signature {
-        unimplemented!();
+        Signature::new(self, message_element)
     }
 }
 
@@ -62,6 +62,7 @@ impl From<PrivateKey> for PublicKey {
 mod tests {
     use super::*;
 
+    #[test]
     fn test_random() {
         let a = PrivateKey::random();
         let b = PrivateKey::random();
@@ -69,11 +70,79 @@ mod tests {
         assert_ne!(a, b);
     }
 
+    #[test]
     fn test_pubkey() {
         let priv_a = PrivateKey::random();
         let pub_a = priv_a.public_key();
 
         assert_eq!(PublicKey::from(priv_a), pub_a);
-        todo!("Get test vectors for the keys");
+        // TODO: Get test vectors (see https://github.com/nucypher/NuBLS/issues/2)
+    }
+
+    #[test]
+    fn test_signing_and_verifying() {
+        let priv_a = PrivateKey::random();
+        let pub_a = priv_a.public_key();
+
+        // Generate and sign a random message to sign in G_2.
+        let rand = PrivateKey::random();
+        let msg = G2Affine::from(G2Affine::generator() * &rand.0);
+
+        let sig_msg = priv_a.sign(&msg);
+        assert_eq!(sig_msg, Signature::new(&priv_a, &msg));
+
+        // Check that the message is valid
+        let verified = pub_a.verify(&msg, &sig_msg);
+        assert_eq!(verified, sig_msg.verify(&pub_a, &msg));
+        assert_eq!(verified, VerificationResult::Valid);
+
+        // Generate a random invalid message for `sig_msg` and check that it
+        // is invalid.
+        let new_rand = PrivateKey::random();
+        let bad_msg = G2Affine::from(G2Affine::generator() * &new_rand.0);
+        assert_ne!(bad_msg, msg);
+
+        let not_verified = pub_a.verify(&bad_msg, &sig_msg);
+        assert_eq!(not_verified, VerificationResult::Invalid);
+    }
+
+    #[test]
+    fn test_verification_result_handling() {
+        // This test demonstrates the misuse-resistant signature verification
+        // API. This is how a user should handle signature verification logic
+        // within their own application.
+        let priv_a = PrivateKey::random();
+        let pub_a = priv_a.public_key();
+
+        // Generate and sign a random message to sign in G_2.
+        let rand = PrivateKey::random();
+        let msg = G2Affine::from(G2Affine::generator() * &rand.0);
+        let sig_msg = priv_a.sign(&msg);
+
+        // We define a function that handles the logic of a signature verification
+        // and returns a string depending on if it verified or not.
+        //
+        // Notice how we use pattern matching to handle the signature verification
+        // result; if we don't handle both cases (Valid/Invalid), this will not compile.
+        // This forces users to properly handle signature verification.
+        fn handle_signature_verification(is_verified: &VerificationResult) -> &str {
+            match is_verified {
+                VerificationResult::Valid => "Valid message!",
+                VerificationResult::Invalid => "Invalid message!",
+            }
+        }
+
+        // Handle a valid signature.
+        let verified = pub_a.verify(&msg, &sig_msg);
+        assert_eq!("Valid message!", handle_signature_verification(&verified));
+
+        // Let's try an invalid signature
+        let new_rand = PrivateKey::random();
+        let bad_msg = G2Affine::from(G2Affine::generator() * &new_rand.0);
+        let not_verified = pub_a.verify(&bad_msg, &sig_msg);
+        assert_eq!(
+            "Invalid message!",
+            handle_signature_verification(&not_verified)
+        );
     }
 }
